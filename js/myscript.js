0ec26dfa2bd4a950437f4be2d935e6b137a0a1e2
YousefKazerooni <yousef@Yousefs-MBP-2.wv.cc.cmu.edu>
2016-10-01 18:23:27 -0400

    created button to show an image in JS

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,30 @@
 document.getElementById("we").style.color = "blue";
 
+var addButton = document.getElementById("add");
+var deleteButton = document.getElementById("delete");
+
+function addTask() {
+  console.log ("add is running");
+
+}
+
+function deleteTask() {
+  console.log ("delete is running");
+
+}
+
+function showImage(){
+  console.log ("show image is running");
+
+  var pic = document.createElement("img");
+
+  pic.setAttribute("src","http://4.bp.blogspot.com/-2Rahyeo7Buw/UilCqtayhaI/AAAAAAAABAQ/WCDeVBDZzkc/s1600/cute_babies_high_quality_4-normal.jpg");
+  pic.setAttribute("id","image");
+
+  console.log(pic);
+  console.log (document.querySelector("#imgContainer"));
+  document.querySelector("#imgContainer").appendChild(pic);
+}
 
 // function fa() {
 //   var headerr= document.getElementById('add');
