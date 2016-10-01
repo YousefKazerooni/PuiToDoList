103918b04a1778f81aec31233222bf4da2e88715
YousefKazerooni <yousef@Yousefs-MBP-2.wv.cc.cmu.edu>
2016-10-01 13:40:39 -0400

    Connected javascript, css, html

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+document.getElementById("we").style.color = "blue";
+
+
+// function fa() {
+//   var headerr= document.getElementById('add');
+// console.log(headerr);
+// var mouse = "house";
+// headerr.parentNode.style.backgroundColor = "red";
+// //addButton.backgroundColor = "orange";
+// // var x1 = headerr.property.style.Color;
+// // console.log(x1);
+// }
+// fa();
