5ce11ea65a74dde8cc6355e468687db87e4ddeb9
YousefKazerooni <yousef@Yousefs-MBP-2.wv.cc.cmu.edu>
2016-10-03 09:24:48 -0400

    first part of remove

--- js/myscript.js
+++ js/myscript.js
@@ -27,20 +27,32 @@ function deleteTask() {
 //   document.querySelector("#imgContainer").appendChild(pic);
 // }
 
-
+//removes an image ; not only parents can remove
+function removeImg(){
+  console.log("remove");
+}
 //Showing alternating images after clicking buttons
 function showImage(imgIdentifier) {
 
   var imgSource;
+  var imgId;
   if (imgIdentifier=="baby"){
       imgSource = "http://4.bp.blogspot.com/-2Rahyeo7Buw/UilCqtayhaI/AAAAAAAABAQ/WCDeVBDZzkc/s1600/cute_babies_high_quality_4-normal.jpg";
+      imgId = "baby";
   } else if (imgIdentifier =="dog"){
       imgSource = "https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg";
+      imgId = "dog";
   }
 
   var pic = document.createElement("img");
   pic.setAttribute("src", imgSource);
+  pic.setAttribute("id", imgId);
   document.querySelector("#imgContainer").appendChild(pic);
+
+  //make the images removable by adding an event listener; remember only parents can Remove
+  pic.addEventListener("click", removeImg);
+
+
 
 
 }
