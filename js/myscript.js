20c8ddf7703bb54566f7f64e97dce02fb439dd17
YousefKazerooni <yousef@Yousefs-MBP-2.wv.cc.cmu.edu>
2016-10-08 12:29:04 -0400

    added bootstrap created Rows and delete Button

--- js/myscript.js
+++ js/myscript.js
@@ -1,17 +1,78 @@
 document.getElementById("we").style.color = "blue";
 
-var addButton = document.getElementById("add");
-var deleteButton = document.getElementById("delete");
+
+
+
+function deleteTask(y) {
+  console.log (y);
+
+}
+
+
+
+//Counter to allow differentiation betw ids for to list rows
+//so that it is easier to delete
+var counterItem = 0;
 
 function addTask() {
-  console.log ("add is running");
+  counterItem++;
+  var counterItemId = counterItem.toString();   //turning counter into a String
+  //console.log(counterItemId);
+
+  //creating bootstrap rows with unique ids using counter
+  var divRow = document.createElement("div");
+  divRow.setAttribute ('class','row');
+  divRow.setAttribute('id',counterItemId);
+  document.querySelector('#todolist').appendChild(divRow);
+
+
+  //creating a paragraph element inside the row dive to display
+  //text area input, delete button, and completion status
+  //Paragraph:
+  var listItems = document.createElement('p');
+  listItems.setAttribute('id', 'inputItems');
+  document.getElementById(counterItemId).appendChild(listItems);
+
+  //delete Button:
+  var delButton = document.createElement('button');
+  var buttonLabel = document.createTextNode ('delete');
+  delButton.appendChild(buttonLabel);
+  delButton.setAttribute('id',counterItemId);           //allows tracking which row to delete in deletTask Function
+  delButton.addEventListener ('click', deleteTask);
+  //delButton.onclick = deletTask; ****to be able to access event properties in the delete function have to use addEventListener
+  document.getElementById(counterItemId).appendChild(delButton);
+
+
+  //completion
+  
+
+
+
+var x = document.getElementById(counterItemId);
+console.log(x);
+
+  // var x = document.getElementById('todolist');
+  // console.log(x);
+  //
+  //
+  // var textInput= document.getElementsByTagName("textarea")[0].value;
+  // console.log(textInput);
+
+
+
+
+  // //console.log(x[0]);
+  // var textCont = textBox.value;
+  // console.log(textCont);
+  // document.querySelector('.items').innerHTML = textCont;
 
 }
 
-function deleteTask() {
-  console.log ("delete is running");
+// <div class= "row">
+//   <p class= "items"></p>
+// </div>
 
-}
+
 
 //showing an image after clicking a button
 // function showImage(){
